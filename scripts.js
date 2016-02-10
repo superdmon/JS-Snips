52f1c361361f39cddb626e3360e72319849fb72c
superdmon <charles@bitstormweb.com>
2016-02-10 08:28:18 -0500

    added smooth scrolling

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
     Useful Javascript snips
 -----------------------------------*/
 
-
+// FULL HEIGHT PAGE ELEMENT
 // Create an element that fills the entire height of the viewport, accurate on mobile devices
 $(window).resize(function(){
    var wfh = 0;
@@ -13,3 +13,21 @@ $(window).resize(function(){
    $('#yourElementGoesHere').height(wfh);
 })
 $(window).resize();
+
+
+// SMOOTH SCROLLING LINKS
+// Allows for smooth window scrolling to named anchor
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
